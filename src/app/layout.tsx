36c3944bfe1d408b355696979ec10819d473e6aa
Hosung99 <shson1217@kw.ac.kr>
2025-04-08 14:20:26 +0900

    fix: mui provider추가적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import ThemeRegistry from "@/components/ThemeRegistry";
 import { LayoutWrapper } from "@/components/LayoutWrapper";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 const notoSans = Noto_Sans_KR({
   variable: "--font-noto-sans",
@@ -26,9 +27,11 @@ export default function RootLayout({
       <body
         className={`${notoSans.variable} antialiased flex flex-col sm:flex-row min-h-screen`}
       >
-        <ThemeRegistry>
-          <LayoutWrapper>{children}</LayoutWrapper>
-        </ThemeRegistry>
+        <AppRouterCacheProvider>
+          <ThemeRegistry>
+            <LayoutWrapper>{children}</LayoutWrapper>
+          </ThemeRegistry>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
