06a63ff36d9fc52bed135cbabc6da6adf8e4562c
Hosung99 <shson1217@kw.ac.kr>
2025-04-08 14:26:41 +0900

    refactor: LayoutWrapper 위치 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import ThemeRegistry from "@/components/ThemeRegistry";
-import { LayoutWrapper } from "@/components/LayoutWrapper";
+import { LayoutWrapper } from "@/containers/LayoutWrapper";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 const notoSans = Noto_Sans_KR({
