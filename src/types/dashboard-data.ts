55f3a039c2338c153cfa344e25a8e19b6716790c
Hosung99 <shson1217@kw.ac.kr>
2025-04-08 19:06:05 +0900

    feat: 대시보드 완성

--- src/types/dashboard-data.ts
+++ src/types/dashboard-data.ts
@@ -2,3 +2,9 @@ export type BarDataItem = {
   value: number | null;
   category: string | null;
 };
+
+export type selectDataItem = {
+  contracter: string[];
+  importer: string[];
+  item: string[];
+};
