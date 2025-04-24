2c61bc9eacce10aa0ce08a4c4f45c0cfc869d589
mgn1440 <mgn1440@gmail.com>
2025-04-24 19:42:53 +0900

    fix: delete test page

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { GridExample } from "@/components/example-grid";
-
-export default function TestPage() {
-  return (
-    <div className="w-full h-screen p-4">
-      <GridExample />
-    </div>
-  );
-}
