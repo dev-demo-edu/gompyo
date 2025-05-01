657b300c968048dcbdda2c039517609ddd86578b
mgn1440 <mgn1440@gmail.com>
2025-05-01 16:06:38 +0900

    feat: 사업자 번호 페이지 구현

---
+++ drizzle/0004_productive_cerise.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `business_numbers` (
+	`id` text PRIMARY KEY NOT NULL,
+	`business_number` text NOT NULL,
+	`business_name` text NOT NULL,
+	`business_representative` text NOT NULL,
+	`created_at` text NOT NULL
+);
---
+++ src/actions/info/business-number-actions.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import {
+  BusinessNumberService,
+  BusinessNumberInput,
+} from "@/services/business-number-service";
+import { nanoid } from "nanoid";
+
+export async function deleteBusinessNumbers(ids: string[]) {
+  await BusinessNumberService.deleteMany(ids);
+}
+
+export type AddBusinessNumberInput = Omit<BusinessNumberInput, "id">;
+
+export async function addBusinessNumber(input: AddBusinessNumberInput) {
+  const businessNumber = input.businessNumber.replace(/-/g, "");
+
+  const existingBusinessNumber =
+    await BusinessNumberService.getByBusinessNumber(businessNumber);
+  if (existingBusinessNumber) {
+    throw new Error("이미 존재하는 사업자번호입니다.");
+  }
+
+  input.businessNumber = businessNumber;
+
+  const newBusinessNumber: BusinessNumberInput = {
+    id: nanoid(),
+    ...input,
+  };
+  await BusinessNumberService.create(newBusinessNumber);
+}
+
+export async function getBusinessNumbers() {
+  const businessNumbers = await BusinessNumberService.getAll();
+  return businessNumbers;
+}
